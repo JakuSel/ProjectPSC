0c18651ea178935da03c8ccba368794f31cf0a62
Jakub Seliga <jakub.seliga@akademiasovy.sk>
2018-04-03 10:03:07 +0200

    Repository iniialization

---
+++ src/main/java/akademiasovy/PSC/ProjectPSCobciApplication.java
@@ -0,0 +1,29 @@
+package akademiasovy.PSC;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class ProjectPSCobciApplication extends Application<ProjectPSCobciConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new ProjectPSCobciApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "ProjectPSCobci";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<ProjectPSCobciConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final ProjectPSCobciConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
