3e56acfa09e5f7d53e29066576cd928161baddce
Jakub Seliga <jakub.seliga@akademiasovy.sk>
2018-04-03 16:24:37 +0200

    Add Web+AJAX requests

--- src/main/java/akademiasovy/PSC/ProjectPSCobciApplication.java
+++ src/main/java/akademiasovy/PSC/ProjectPSCobciApplication.java
@@ -4,6 +4,11 @@ import akademiasovy.PSC.resources.Locations;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class ProjectPSCobciApplication extends Application<ProjectPSCobciConfiguration> {
 
@@ -29,6 +34,15 @@ public class ProjectPSCobciApplication extends Application<ProjectPSCobciConfigu
                 new Locations()
         );
 
-    }
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
 
+// Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+// Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+    }
 }
